b497c459a1a6f41ee6482e4b90269c069550d9fa
invict224 <invict224@gmail.com>
2024-10-21 22:15:20 +0900

    asdasd

--- scripts.js
+++ scripts.js
@@ -11,7 +11,10 @@ window.onload = function() {
             const appSecret = 'f58acdd049c575d670a0c1d06d2f14c0e03ac7407f815acbe482bf311c2b622dea92e9e0959d7307a19a39a56aff782e1c2f62c6b4ff4401e1718b1fddd42eba72db0e63bdf9f6ef4b9a63345a8b8c43fd5e6560887b6dd788e580432dc5b453e56f4224b6d194f560f2c43589191d8b';
             const authString = btoa(`${appId}:${appSecret}`);
 
-            const url = `https://api.astronomyapi.com/api/v2/bodies/positions?longitude=${longitude}&latitude=${latitude}&elevation=1&from_date=today&to_date=today&time=22%3A08%3A56`;
+            const today = new Date().toISOString().split('T')[0];  // Get today's date in YYYY-MM-DD format
+            const time = new Date().toISOString().split('T')[1].split('.')[0];  // Get current time in HH:MM:SS format
+
+            const url = `https://api.astronomyapi.com/api/v2/bodies/positions?longitude=${longitude}&latitude=${latitude}&elevation=1&from_date=${today}&to_date=${today}&time=${time}`;
 
             fetch(url, {
                 method: 'GET',
@@ -19,8 +22,14 @@ window.onload = function() {
                     'Authorization': `Basic ${authString}`
                 }
             })
-                .then(response => response.json())
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
                 .then(marsData => {
+                    console.log('Mars Data:', marsData);
                     const marsPosition = marsData.data.table.rows[0].cells;
                     document.getElementById('mars-info').innerHTML = `Mars is currently at Right Ascension: ${marsPosition[0].value}, Declination: ${marsPosition[1].value}.`;
                 })
